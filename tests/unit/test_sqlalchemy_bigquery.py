3b04019a9894cb7c4a15a1c405ae3d7fbd9b8ec6
Tim Swast <swast@google.com>
2021-03-31 16:10:28 -0500

    fix: avoid 404 if dataset is deleted while listing tables or views

---
+++ tests/unit/test_sqlalchemy_bigquery.py
@@ -0,0 +1,13 @@
+# Copyright 2021 The PyBigQuery Authors
+#
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
+
+def test_get_table_names():
+    assert False
+
+
+def test_get_view_names():
+    assert False
