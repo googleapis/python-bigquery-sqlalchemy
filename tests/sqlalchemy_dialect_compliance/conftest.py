497cc905efa718477b329509a4164b8340664040
Jim Fulton <jim@jimfulton.info>
2021-04-26 10:28:52 -0600

    Ignore flake8 complaint about * import.
    The names are known and used by pytest.

--- tests/sqlalchemy_dialect_compliance/conftest.py
+++ tests/sqlalchemy_dialect_compliance/conftest.py
@@ -17,7 +17,7 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-from sqlalchemy.testing.plugin.pytestplugin import *
+from sqlalchemy.testing.plugin.pytestplugin import *  # noqa
 
 import google.cloud.bigquery.dbapi.connection
 import pybigquery.sqlalchemy_bigquery
