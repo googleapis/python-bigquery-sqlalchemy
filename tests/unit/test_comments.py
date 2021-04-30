8236d5bbe7fc653d88372d5e66d34267bd76bc26
Jim Fulton <jim@jimfulton.info>
2021-04-30 12:10:32 -0600

    test dialect-options handling.

--- tests/unit/test_comments.py
+++ tests/unit/test_comments.py
@@ -15,7 +15,6 @@ def test_inline_comments(faux_conn):
     assert dialect.get_columns(faux_conn, "some_table")[0]['comment'] == 'identifier'
 
 def test_set_drop_table_comment(faux_conn):
-
     metadata = sqlalchemy.MetaData()
     table = sqlalchemy.Table(
         "some_table",
@@ -33,3 +32,47 @@ def test_set_drop_table_comment(faux_conn):
 
     faux_conn.execute(sqlalchemy.schema.DropTableComment(table))
     assert dialect.get_table_comment(faux_conn, "some_table") == {'text': None}
+
+
+def test_table_description_dialect_option(faux_conn):
+    metadata = sqlalchemy.MetaData()
+    table = sqlalchemy.Table(
+        "some_table",
+        metadata,
+        sqlalchemy.Column("id", sqlalchemy.Integer),
+        bigquery_description="a fine table",
+    )
+    metadata.create_all(faux_conn.engine)
+    dialect = faux_conn.dialect
+    assert dialect.get_table_comment(faux_conn, "some_table") == {'text': 'a fine table'}
+
+def test_table_friendly_name_dialect_option(faux_conn):
+    metadata = sqlalchemy.MetaData()
+    table = sqlalchemy.Table(
+        "some_table",
+        metadata,
+        sqlalchemy.Column("id", sqlalchemy.Integer),
+        bigquery_friendly_name="bob",
+    )
+    metadata.create_all(faux_conn.engine)
+
+    assert ' '.join(faux_conn.test_data["execute"][-1][0].strip().split()
+                    ) == ("CREATE TABLE `some_table` ( `id` INT64 )"
+                          " OPTIONS(friendly_name='bob')")
+
+def test_table_friendly_name_description_dialect_option(faux_conn):
+    metadata = sqlalchemy.MetaData()
+    table = sqlalchemy.Table(
+        "some_table",
+        metadata,
+        sqlalchemy.Column("id", sqlalchemy.Integer),
+        bigquery_friendly_name="bob",
+        bigquery_description="a fine table",
+    )
+    metadata.create_all(faux_conn.engine)
+
+    dialect = faux_conn.dialect
+    assert dialect.get_table_comment(faux_conn, "some_table") == {'text': 'a fine table'}
+    assert ' '.join(faux_conn.test_data["execute"][-1][0].strip().split()
+                    ) == ("CREATE TABLE `some_table` ( `id` INT64 )"
+                          " OPTIONS(description='a fine table', friendly_name='bob')")
