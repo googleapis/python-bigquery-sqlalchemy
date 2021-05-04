5cf4009776c3fd71a8dd68aa4745a57f2dd952e6
Jim Fulton <jim@jimfulton.info>
2021-05-04 10:52:52 -0600

    ported test for %%s

--- tests/unit/test_compliance.py
+++ tests/unit/test_compliance.py
@@ -18,12 +18,12 @@ Mainly to get better unit test coverage.
 """
 
 import sqlalchemy
-from sqlalchemy import Column, Integer, select, union
+from sqlalchemy import Column, Integer, literal_column, select, String, Table, union
 from sqlalchemy.testing.assertions import eq_
 
 def setup_table(connection, name, *columns, initial_data=(), **kw):
     metadata = sqlalchemy.MetaData()
-    table = sqlalchemy.Table(name, metadata, *columns, **kw)
+    table = Table(name, metadata, *columns, **kw)
     metadata.create_all(connection.engine)
     if initial_data:
         connection.execute(table.insert(), initial_data)
@@ -33,9 +33,9 @@ def assert_result(connection, sel, expected):
     eq_(connection.execute(sel).fetchall(), expected)
 
 
-def test_distinct_selectable_in_unions(faux_conn):
-    table = setup_table(
-        faux_conn,
+def some_table(connection):
+    return setup_table(
+        connection,
         "some_table",
         Column("id", Integer),
         Column("x", Integer),
@@ -47,8 +47,62 @@ def test_distinct_selectable_in_unions(faux_conn):
             {"id": 4, "x": 4, "y": 5},
             ]
         )
+
+def test_distinct_selectable_in_unions(faux_conn):
+    table = some_table(faux_conn)
     s1 = select([table]).where(table.c.id == 2).distinct()
     s2 = select([table]).where(table.c.id == 3).distinct()
 
     u1 = union(s1, s2).limit(2)
     assert_result(faux_conn, u1.order_by(u1.c.id), [(2, 2, 3), (3, 3, 4)])
+
+
+def test_limit_offset_aliased_selectable_in_unions(faux_conn):
+    table = some_table(faux_conn)
+    s1 = (
+        select([table])
+        .where(table.c.id == 2)
+        .limit(1)
+        .order_by(table.c.id)
+        .alias()
+        .select()
+    )
+    s2 = (
+        select([table])
+        .where(table.c.id == 3)
+        .limit(1)
+        .order_by(table.c.id)
+        .alias()
+        .select()
+    )
+
+    u1 = union(s1, s2).limit(2)
+    assert_result(faux_conn, u1.order_by(u1.c.id), [(2, 2, 3), (3, 3, 4)])
+
+
+def test_percent_sign_round_trip(faux_conn, metadata):
+    """test that the DBAPI accommodates for escaped / nonescaped
+    percent signs in a way that matches the compiler
+
+    """
+    t = Table("t", metadata, Column("data", String(50)))
+    t.create(faux_conn.engine)
+    faux_conn.execute(t.insert(), dict(data="some % value"))
+    faux_conn.execute(t.insert(), dict(data="some %% other value"))
+    eq_(
+        faux_conn.scalar(
+            select([t.c.data]).where(
+                t.c.data == literal_column("'some % value'")
+                )
+            ),
+        "some % value",
+        )
+
+    eq_(
+        faux_conn.scalar(
+            select([t.c.data]).where(
+                t.c.data == literal_column("'some %% other value'")
+                )
+            ),
+        "some %% other value",
+        )
