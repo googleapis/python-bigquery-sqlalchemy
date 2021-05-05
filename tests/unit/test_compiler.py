a65bd1ab5e7bc3ebb67560cef56874c0414419f4
Jim Fulton <jim@jimfulton.info>
2021-05-05 08:30:25 -0600

    Added a test for compiling a single column.
    Also,simplified other test using the metadata helper.

--- tests/unit/test_compiler.py
+++ tests/unit/test_compiler.py
@@ -1,14 +1,12 @@
 import sqlalchemy
 
+from conftest import setup_table
 
-def test_constraints_are_ignored(faux_conn):
 
-    metadata = sqlalchemy.MetaData()
-
+def test_constraints_are_ignored(faux_conn, metadata):
     sqlalchemy.Table(
         "ref", metadata, sqlalchemy.Column("id", sqlalchemy.Integer),
     )
-
     sqlalchemy.Table(
         "some_table",
         metadata,
@@ -18,9 +16,12 @@ def test_constraints_are_ignored(faux_conn):
         ),
         sqlalchemy.UniqueConstraint("id", "ref_id", name="uix_1"),
     )
-
     metadata.create_all(faux_conn.engine)
-
     assert " ".join(faux_conn.test_data["execute"][-1][0].strip().split()) == (
         "CREATE TABLE `some_table`" " ( `id` INT64 NOT NULL, `ref_id` INT64 )"
     )
+
+
+def test_compile_column(faux_conn):
+    table = setup_table(faux_conn, "t", sqlalchemy.Column("c", sqlalchemy.Integer))
+    assert table.c.c.compile(faux_conn).string == "`c`"
