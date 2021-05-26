95b0692e9b83290b5f0bb45f7dd49f9a10bba5e8
yoshi-automation <yoshi-automation@google.com>
2021-05-26 04:21:08 +0000

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- noxfile.py
+++ noxfile.py
@@ -28,20 +28,17 @@ BLACK_VERSION = "black==19.10b0"
 BLACK_PATHS = ["docs", "pybigquery", "tests", "noxfile.py", "setup.py"]
 
 DEFAULT_PYTHON_VERSION = "3.8"
-
-# We're using two Python versions to test with sqlalchemy 1.3 and 1.4.
-SYSTEM_TEST_PYTHON_VERSIONS = ["3.8", "3.9"]
+SYSTEM_TEST_PYTHON_VERSIONS = ["3.8"]
 UNIT_TEST_PYTHON_VERSIONS = ["3.6", "3.7", "3.8", "3.9"]
 
 CURRENT_DIRECTORY = pathlib.Path(__file__).parent.absolute()
 
 # 'docfx' is excluded since it only needs to run in 'docs-presubmit'
 nox.options.sessions = [
-    "lint",
     "unit",
-    "cover",
     "system",
-    "compliance",
+    "cover",
+    "lint",
     "lint_setup_py",
     "blacken",
     "docs",
@@ -49,7 +46,6 @@ nox.options.sessions = [
 
 # Error if a python version is missing
 nox.options.error_on_missing_interpreters = True
-nox.options.stop_on_first_error = True
 
 
 @nox.session(python=DEFAULT_PYTHON_VERSION)
@@ -82,22 +78,6 @@ def lint_setup_py(session):
     session.run("python", "setup.py", "check", "--restructuredtext", "--strict")
 
 
-def install_alembic_for_python_38(session, constraints_path):
-    """
-    install alembic for Python 3.8 unit and system tests
-
-    We don't require alembic and most tests should run without it, however
-
-    - We run some unit tests (Python 3.8) to cover the alembic
-      registration that happens when alembic is installed.
-
-    - We have a system test that demonstrates working with alembic and
-      proves that the things we think should work do work. :)
-    """
-    if session.python == "3.8":
-        session.install("alembic", "-c", constraints_path)
-
-
 def default(session):
     # Install all test dependencies, then install this package in-place.
 
@@ -105,7 +85,7 @@ def default(session):
         CURRENT_DIRECTORY / "testing" / f"constraints-{session.python}.txt"
     )
     session.install("mock", "pytest", "pytest-cov", "-c", constraints_path)
-    install_alembic_for_python_38(session, constraints_path)
+
     session.install("-e", ".", "-c", constraints_path)
 
     # Run py.test against the unit tests.
@@ -158,7 +138,6 @@ def system(session):
     # Install all test dependencies, then install this package into the
     # virtualenv's dist-packages.
     session.install("mock", "pytest", "google-cloud-testutils", "-c", constraints_path)
-    install_alembic_for_python_38(session, constraints_path)
     session.install("-e", ".", "-c", constraints_path)
 
     # Run py.test against the system tests.
@@ -180,57 +159,6 @@ def system(session):
         )
 
 
-@nox.session(python=SYSTEM_TEST_PYTHON_VERSIONS)
-def compliance(session):
-    """Run the system test suite."""
-    constraints_path = str(
-        CURRENT_DIRECTORY / "testing" / f"constraints-{session.python}.txt"
-    )
-    system_test_folder_path = os.path.join("tests", "sqlalchemy_dialect_compliance")
-
-    # Check the value of `RUN_SYSTEM_TESTS` env var. It defaults to true.
-    if os.environ.get("RUN_COMPLIANCE_TESTS", "true") == "false":
-        session.skip("RUN_COMPLIANCE_TESTS is set to false, skipping")
-    # Sanity check: Only run tests if the environment variable is set.
-    if not os.environ.get("GOOGLE_APPLICATION_CREDENTIALS", ""):
-        session.skip("Credentials must be set via environment variable")
-    # Install pyopenssl for mTLS testing.
-    if os.environ.get("GOOGLE_API_USE_CLIENT_CERTIFICATE", "false") == "true":
-        session.install("pyopenssl")
-    # Sanity check: only run tests if found.
-    if not os.path.exists(system_test_folder_path):
-        session.skip("Compliance tests were not found")
-
-    # Use pre-release gRPC for system tests.
-    session.install("--pre", "grpcio")
-
-    # Install all test dependencies, then install this package into the
-    # virtualenv's dist-packages.
-    session.install(
-        "mock",
-        "pytest",
-        "pytest-rerunfailures",
-        "google-cloud-testutils",
-        "-c",
-        constraints_path,
-    )
-    session.install("-e", ".", "-c", constraints_path)
-
-    session.run(
-        "py.test",
-        "-vv",
-        f"--junitxml=compliance_{session.python}_sponge_log.xml",
-        "--reruns=3",
-        "--reruns-delay=60",
-        "--only-rerun=403 Exceeded rate limits",
-        "--only-rerun=409 Already Exists",
-        "--only-rerun=404 Not found",
-        "--only-rerun=400 Cannot execute DML over a non-existent table",
-        system_test_folder_path,
-        *session.posargs,
-    )
-
-
 @nox.session(python=DEFAULT_PYTHON_VERSION)
 def cover(session):
     """Run the final coverage report.
@@ -239,7 +167,7 @@ def cover(session):
     test runs (not system test runs), and then erases coverage data.
     """
     session.install("coverage", "pytest-cov")
-    session.run("coverage", "report", "--show-missing", "--fail-under=100")
+    session.run("coverage", "report", "--show-missing", "--fail-under=50")
 
     session.run("coverage", "erase")
 
@@ -249,7 +177,7 @@ def docs(session):
     """Build the docs for this library."""
 
     session.install("-e", ".")
-    session.install("sphinx", "alabaster", "recommonmark")
+    session.install("sphinx==4.0.1", "alabaster", "recommonmark")
 
     shutil.rmtree(os.path.join("docs", "_build"), ignore_errors=True)
     session.run(
@@ -271,7 +199,9 @@ def docfx(session):
     """Build the docfx yaml files for this library."""
 
     session.install("-e", ".")
-    session.install("sphinx", "alabaster", "recommonmark", "gcp-sphinx-docfx-yaml")
+    session.install(
+        "sphinx==4.0.1", "alabaster", "recommonmark", "gcp-sphinx-docfx-yaml"
+    )
 
     shutil.rmtree(os.path.join("docs", "_build"), ignore_errors=True)
     session.run(
