0df170128ba2b4d0fe91340b2fdfe58f09fa1e96
Jim Fulton <jim@jimfulton.info>
2021-08-31 16:18:09 -0600

    Added system tests to verift PR 67 and issue 233

--- tests/system/test__struct.py
+++ tests/system/test__struct.py
@@ -89,3 +89,96 @@ def test_struct(engine, bigquery_dataset, metadata):
         )
         == []
     )
+
+
+def test_complex_literals_pr_67(engine, bigquery_dataset, metadata):
+    # https://github.com/googleapis/python-bigquery-sqlalchemy/pull/67
+
+    # Simple select example:
+
+    table_name = f"{bigquery_dataset}.test_comples_literals_pr_67"
+    engine.execute(
+        f"""
+        create table {table_name} as (
+            select 'a' as id,
+            struct(1 as x__count, 2 as y__count, 3 as z__count) as dimensions
+            )
+        """)
+
+    table = sqlalchemy.Table(table_name, metadata, autoload_with=engine)
+
+    got = str(sqlalchemy.select([(table.c.dimensions.x__count + 5).label("c")])
+              .compile(engine))
+    want = (
+        f"SELECT (`{table_name}`.`dimensions`.x__count) + %(param_1:INT64)s AS `c` \n"
+        f"FROM `{table_name}`"
+        )
+
+    assert got == want
+
+    # Hopefully, "Example doing a pivot" is addressed by
+    # test_unnest_and_struct_access_233 below :)
+
+
+def test_unnest_and_struct_access_233(engine, bigquery_dataset, metadata):
+    # https://github.com/googleapis/python-bigquery-sqlalchemy/issues/233
+
+    from sqlalchemy import Table, select, Column, ARRAY, String, func
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy_bigquery import STRUCT
+
+    conn = engine.connect()
+
+    mock_table = Table(f"{bigquery_dataset}.Mock", metadata, Column("mock_id", String))
+    another_mock_table = Table(
+        f"{bigquery_dataset}.AnotherMock",
+        metadata,
+        Column("objects", ARRAY(STRUCT(object_id=String))),
+        )
+    metadata.create_all(engine)
+
+    conn.execute(
+        mock_table.insert(),
+        dict(mock_id="x"),
+        dict(mock_id="y"),
+        dict(mock_id="z"),
+    )
+    conn.execute(
+        another_mock_table.insert(),
+        dict(objects=[dict(object_id='x'), dict(object_id='y'), dict(object_id='q')]),
+    )
+
+    subquery = select(
+        func.unnest(another_mock_table.c.objects).alias('another_mock_objects').column
+        ).subquery()
+
+    join = mock_table.join(
+        subquery,
+        subquery.c.another_mock_objects['object_id'] == mock_table.c.mock_id,
+        )
+
+    query = select(mock_table).select_from(join)
+
+    got = str(query.compile(engine))
+    want = (
+        f"SELECT `{bigquery_dataset}.Mock`.`mock_id` \n"
+        f"FROM `{bigquery_dataset}.Mock` "
+        f"JOIN ("
+        f"SELECT `another_mock_objects` \n"
+        f"FROM "
+        f"`{bigquery_dataset}.AnotherMock` `{bigquery_dataset}.AnotherMock_1`, "
+        f"unnest(`{bigquery_dataset}.AnotherMock_1`.`objects`)"
+        f" AS `another_mock_objects`"
+        f") AS `anon_1` "
+        f"ON "
+        f"(`anon_1`.`another_mock_objects`.object_id) = "
+        f"`{bigquery_dataset}.Mock`.`mock_id`"
+        )
+    assert got == want
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    results = sorted(session.execute(query))
+
+    assert results == [('x',), ('y',)]
+
