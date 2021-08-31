f10a571b2e2fec732c87145705da2be91ed3450a
Jim Fulton <jim@jimfulton.info>
2021-08-31 16:22:44 -0600

    blacken

--- tests/system/test__struct.py
+++ tests/system/test__struct.py
@@ -103,16 +103,20 @@ def test_complex_literals_pr_67(engine, bigquery_dataset, metadata):
             select 'a' as id,
             struct(1 as x__count, 2 as y__count, 3 as z__count) as dimensions
             )
-        """)
+        """
+    )
 
     table = sqlalchemy.Table(table_name, metadata, autoload_with=engine)
 
-    got = str(sqlalchemy.select([(table.c.dimensions.x__count + 5).label("c")])
-              .compile(engine))
+    got = str(
+        sqlalchemy.select([(table.c.dimensions.x__count + 5).label("c")]).compile(
+            engine
+        )
+    )
     want = (
         f"SELECT (`{table_name}`.`dimensions`.x__count) + %(param_1:INT64)s AS `c` \n"
         f"FROM `{table_name}`"
-        )
+    )
 
     assert got == want
 
@@ -134,28 +138,24 @@ def test_unnest_and_struct_access_233(engine, bigquery_dataset, metadata):
         f"{bigquery_dataset}.AnotherMock",
         metadata,
         Column("objects", ARRAY(STRUCT(object_id=String))),
-        )
+    )
     metadata.create_all(engine)
 
     conn.execute(
-        mock_table.insert(),
-        dict(mock_id="x"),
-        dict(mock_id="y"),
-        dict(mock_id="z"),
+        mock_table.insert(), dict(mock_id="x"), dict(mock_id="y"), dict(mock_id="z"),
     )
     conn.execute(
         another_mock_table.insert(),
-        dict(objects=[dict(object_id='x'), dict(object_id='y'), dict(object_id='q')]),
+        dict(objects=[dict(object_id="x"), dict(object_id="y"), dict(object_id="q")]),
     )
 
     subquery = select(
-        func.unnest(another_mock_table.c.objects).alias('another_mock_objects').column
-        ).subquery()
+        func.unnest(another_mock_table.c.objects).alias("another_mock_objects").column
+    ).subquery()
 
     join = mock_table.join(
-        subquery,
-        subquery.c.another_mock_objects['object_id'] == mock_table.c.mock_id,
-        )
+        subquery, subquery.c.another_mock_objects["object_id"] == mock_table.c.mock_id,
+    )
 
     query = select(mock_table).select_from(join)
 
@@ -173,12 +173,11 @@ def test_unnest_and_struct_access_233(engine, bigquery_dataset, metadata):
         f"ON "
         f"(`anon_1`.`another_mock_objects`.object_id) = "
         f"`{bigquery_dataset}.Mock`.`mock_id`"
-        )
+    )
     assert got == want
 
     Session = sessionmaker(bind=engine)
     session = Session()
     results = sorted(session.execute(query))
 
-    assert results == [('x',), ('y',)]
-
+    assert results == [("x",), ("y",)]
