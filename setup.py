1a82100cddb798c5df01be3f545037a4a4f0efb9
Anthonios Partheniou <partheniou@google.com>
2022-07-10 13:26:45 -0400

    update python_requires in setup.py

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ setup(
         "future",
     ],
     extras_require=extras,
-    python_requires=">=3.6, <3.11",
+    python_requires=">=3.7, <3.11",
     tests_require=["packaging", "pytz"],
     entry_points={
         "sqlalchemy.dialects": ["bigquery = sqlalchemy_bigquery:BigQueryDialect"]
