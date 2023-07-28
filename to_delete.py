80918c0e04a8445ac8d8744dc96faea85c9693cd
Chalmer Lowe <chalmerlowe@google.com>
2023-07-28 08:48:51 -0400

    Delete to_delete.py
    inadvertently uploaded.

--- to_delete.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import literal, select
-from sqlalchemy_bigquery.base import BigQueryDialect
-query = select(literal(1).label("a:b"))
-
-print(query.compile(compile_kwargs={"literal_binds": True}, dialect=BigQueryDialect()))
-
