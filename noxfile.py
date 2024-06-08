1320455ac08282271ea4b6dc5218902e0d22f45e
Linchin <lingqing.gan@gmail.com>
2024-06-07 17:57:44 -0700

    include third_party in pytest

--- noxfile.py
+++ noxfile.py
@@ -225,6 +225,7 @@ def default(session, install_extras=True):
         "--cov-report=",
         "--cov-fail-under=0",
         os.path.join("tests", "unit"),
+        os.path.join("third_party", "sqlalchemy_bigquery_vendored"),
         *session.posargs,
     )
 
