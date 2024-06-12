3c94acac20aa179b1b563a2685549f6734fa79ea
Linchin <lingqing.gan@gmail.com>
2024-06-11 17:12:33 -0700

    undo changes for testing

--- noxfile.py
+++ noxfile.py
@@ -43,6 +43,7 @@ LINT_PATHS = [
 DEFAULT_PYTHON_VERSION = "3.8"
 
 UNIT_TEST_PYTHON_VERSIONS: List[str] = ["3.8", "3.9", "3.10", "3.11", "3.12"]
+
 UNIT_TEST_STANDARD_DEPENDENCIES = [
     "mock",
     "asyncmock",
